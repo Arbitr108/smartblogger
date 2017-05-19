e3ba98c108346b3d843901b82af5269c18d86435
PavelGora <spec4web@gmail.com>
2017-05-19 17:25:13 +0300

    added database dump

---
+++ docs/developer.sql
@@ -0,0 +1,73 @@
+/*
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 5.6.36-82.0 : Database - dev.smartdeveloper
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dev.smartdeveloper` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `dev.smartdeveloper`;
+
+/*Table structure for table `comment` */
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `text` text,
+  `user_id` int(11) unsigned NOT NULL,
+  `parent_comment_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comment_user_id_user_user_id_fk` (`user_id`),
+  KEY `comment_comment_id_comment_id_fk` (`parent_comment_id`),
+  CONSTRAINT `comment_comment_id_comment_id_fk` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment` (`id`),
+  CONSTRAINT `comment_user_id_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `comment` */
+
+/*Table structure for table `post` */
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `text` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`),
+  KEY `post_user_id_user_id_fk` (`user_id`),
+  CONSTRAINT `post_user_id_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `post` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `session` varchar(40) DEFAULT NULL,
+  `disabled` tinyint(1) unsigned DEFAULT '0',
+  `created_dt` datetime DEFAULT '0000-00-00 00:00:00',
+  `modified_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_idx` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
